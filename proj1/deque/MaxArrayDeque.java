4f76ed5b5f1831ab66ba4d919a8d3a07dbf82cf4
FlowerseaHao <1052094815@qq.com>
2024-10-24 12:45:43 +0800

    Update MaxArrayDeque.java

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -2,16 +2,24 @@ package deque;
 
 import java.util.Comparator;
 public class MaxArrayDeque<T> extends ArrayDeque<T>{
-    public MaxArrayDeque(Comparator<T> c){
+    Comparator<T> maxComparator;
 
+    public MaxArrayDeque(Comparator<T> c){
+        maxComparator = c;
     }
 
     public T max(){
-
+        return max(maxComparator);
     }
 
     public T max(Comparator<T> c){
-
+        T maxValue = get(0);
+        for(int i = 1;i<size();i++){
+            if(c.compare(get(i),maxValue) > 0){
+                maxValue = get(i);
+            }
+        }
+        return maxValue;
     }
 
     private static class IntegerComparator implements Comparator<Integer>{
@@ -22,5 +30,26 @@ public class MaxArrayDeque<T> extends ArrayDeque<T>{
         }
     }
 
-    
+    private static class StringComparator implements Comparator<String>{
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    private static class FloatComparator implements Comparator<Float>{
+
+        @Override
+        public int compare(Float o1, Float o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    private static class DoubleComparator implements Comparator<Double>{
+
+        @Override
+        public int compare(Double o1, Double o2) {
+            return o1.compareTo(o2);
+        }
+    }
 }
